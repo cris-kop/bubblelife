8c3faa2b4bcf0aeb8e18f96538b87ff647d1615e
cozzie4d <cris@sierracosworth.nl>
2025-01-26 15:03:13 +0100

    Working build for upload no highscores

--- Assets/Scripts/ContinueButton.cs
+++ Assets/Scripts/ContinueButton.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class ContinueButton : MonoBehaviour
 {
+    public void Update()
+    {
+        if (Input.GetButton("StartGame"))
+        {
+            OnContinueButtonClicked();
+        }
+    }
     public void OnContinueButtonClicked()
     {
         SceneManager.LoadScene("homescreen");
